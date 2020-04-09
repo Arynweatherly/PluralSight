8e9ef7b647809364cf824147311d02b7053e3a1c
aryn weatherly <arynweatherly93@gmail.com>
2020-04-09 11:52:45 -0500

    part 2 done

--- Program.cs
+++ Program.cs
@@ -10,16 +10,15 @@ namespace GradeBook
         {
             //variable declaration 
             //declare variable x to hold a floating point value 
+// initialization expression
+            double[] numbers = new[] {12.7, 10.3, 6.11}; //array can hold 3 floating point numbers
+            var result = 0.0;
+            foreach(var number in numbers) 
+            {
+                result += number;
+            }
+Console.WriteLine(result);
 
-            double[] numbers = new double[3]; //array can hold 3 floating point numbers
-            numbers[0] = 12.7;
-            numbers[1] = 10.3;
-            numbers[2] = 6.11;
-
-            var result = numbers[0];
-            result = result + numbers[1];
-            result = result + numbers[2];
-            Console.WriteLine(result);
           
          if(args.Length > 0)
          {
