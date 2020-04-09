6a2c091e74fec21c3326616eded8c593339ddb4a
aryn weatherly <arynweatherly93@gmail.com>
2020-04-09 12:32:00 -0500

    used List

--- Program.cs
+++ Program.cs
@@ -1,19 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
     class Program
     {
-        //the following is a method and the stuff in the curlies get executed 
-
         static void Main(string[] args)
         {
-            //variable declaration 
-            //declare variable x to hold a floating point value 
-// initialization expression
-            double[] numbers = new[] {12.7, 10.3, 6.11}; //array can hold 3 floating point numbers
+            
+            var numbers = new[] {12.7, 10.3, 6.11, 4.1}; //array can hold 3 floating point numbers
+           //initialization syntax
+           var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
+           grades.Add(56.1);
+           
             var result = 0.0;
-            foreach(var number in numbers) 
+            //iterate over the lsit
+            foreach(var number in grades) 
             {
                 result += number;
             }
