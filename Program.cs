f1a89fc7e958da9909a8572bfd8ad2759659c9d5
aryn weatherly <arynweatherly93@gmail.com>
2020-04-09 09:08:55 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        //the following is a method and the stuff in the curlies get executed 
+
+        static void Main(string[] args)
+        {
+         if(args.Length > 0)
+         {
+            Console.WriteLine($"Hello,  {args[0]} !");
+        }
+        else
+         {
+            Console.WriteLine("Hello!");
+        }
+    }
+}
+}
