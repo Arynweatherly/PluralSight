ea6426196248780be4b8e623bcd997b7fbc53e4c
aryn weatherly <arynweatherly93@gmail.com>
2020-04-09 17:10:13 -0500

    done with module 2 of grade keeper

--- Program.cs
+++ Program.cs
@@ -1,4 +1,45 @@
-﻿using System;
+﻿// using System;
+// using System.Collections.Generic;
+
+// namespace GradeBook
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+            
+//             var numbers = new[] {12.7, 10.3, 6.11, 4.1}; //array can hold 3 floating point numbers
+//            //initialization syntax
+//            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
+//            grades.Add(56.1);
+           
+//             var result = 0.0;
+//             //iterate over the lsit
+//             foreach(var number in grades) 
+//             {
+//                 result += number;
+//             }
+// Console.WriteLine(result);
+
+          
+//          if(args.Length > 0)
+//          {
+//             Console.WriteLine($"Hello,  {args[0]} !");
+//         }
+//         else
+//          {
+//             Console.WriteLine("Hello!");
+//         }
+//     }
+// }
+// }
+
+
+
+
+//compute average grade:
+
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook
@@ -8,7 +49,6 @@ namespace GradeBook
         static void Main(string[] args)
         {
             
-            var numbers = new[] {12.7, 10.3, 6.11, 4.1}; //array can hold 3 floating point numbers
            //initialization syntax
            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
            grades.Add(56.1);
@@ -19,7 +59,8 @@ namespace GradeBook
             {
                 result += number;
             }
-Console.WriteLine(result);
+            result /= grades.Count;
+Console.WriteLine($"The average grade is {result:N1}");
 
           
          if(args.Length > 0)
