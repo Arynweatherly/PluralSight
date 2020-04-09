59e5488a046ecccc56f3b6263cdaa6336c1db7ec
aryn weatherly <arynweatherly93@gmail.com>
2020-04-09 11:27:46 -0500

    added floating point numbers

--- Program.cs
+++ Program.cs
@@ -8,6 +8,19 @@ namespace GradeBook
 
         static void Main(string[] args)
         {
+            //variable declaration 
+            //declare variable x to hold a floating point value 
+
+            double[] numbers = new double[3]; //array can hold 3 floating point numbers
+            numbers[0] = 12.7;
+            numbers[1] = 10.3;
+            numbers[2] = 6.11;
+
+            var result = numbers[0];
+            result = result + numbers[1];
+            result = result + numbers[2];
+            Console.WriteLine(result);
+          
          if(args.Length > 0)
          {
             Console.WriteLine($"Hello,  {args[0]} !");
