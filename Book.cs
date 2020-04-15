fc0d2b0985e2cc3daf119dabf4948792f49cfe3b
aryn weatherly <arynweatherly93@gmail.com>
2020-04-15 11:32:27 -0500

    added constructor method

--- Book.cs
+++ Book.cs
@@ -1,9 +1,19 @@
+using System.Collections.Generic;
+
 namespace GradeBook{
     class Book 
-    { //make method for adding a grade . type is double and name of parameter is grade
-        public void AddGrade(double grade) 
+    { 
+        //constructor
+        public Book()
         {
             
         }
+        //make method for adding a grade . type is double and name of parameter is grade
+    
+        public void AddGrade(double grade) 
+        {
+            grades.Add(grade);
+        }
+        List<double> grades;
     }
 }
