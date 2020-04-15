a97e458aec7284242025f3ecc121330809e19b67
aryn weatherly <arynweatherly93@gmail.com>
2020-04-15 11:06:57 -0500

    added the Book class and file

--- Program.cs
+++ Program.cs
@@ -44,11 +44,12 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
+    //define a new class 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            var book = new Book();
            //initialization syntax
            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
            grades.Add(56.1);
@@ -59,7 +60,7 @@ namespace GradeBook
             {
                 result += number;
             }
-            result /= grades.Count;
+            result /= grades.Count; //
 Console.WriteLine($"The average grade is {result:N1}");
 
           
