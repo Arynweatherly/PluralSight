52965d58582f0f075652f7af9480bec1b1e27967
aryn weatherly <arynweatherly93@gmail.com>
2020-04-15 11:54:28 -0500

    found highest grade

--- Program.cs
+++ Program.cs
@@ -49,21 +49,31 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+         
+
             var book = new Book("Aryn's Gradebook");
             book.AddGrade(89.1);
+            book.AddGrade(90.5);
+
             
            //initialization syntax
            var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
            grades.Add(56.1);
            
             var result = 0.0;
+
+            //want to find highest grade in this list 
+            var highGrade = double.MinValue;
             //iterate over the lsit
             foreach(var number in grades) 
             {
+                if(number > highGrade) {
+                    highGrade = number;
+                }
                 result += number;
             }
             result /= grades.Count; 
-Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The average grade is {result:N1}");
 
           
 
