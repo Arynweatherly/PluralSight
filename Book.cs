a241526c7e9a7597070fba889bdd1356150d5a76
aryn weatherly <arynweatherly93@gmail.com>
2020-04-15 12:06:00 -0500

    finished module 4

--- Book.cs
+++ Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace GradeBook{
@@ -15,6 +16,26 @@ namespace GradeBook{
         {
             grades.Add(grade);
         }
+   
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach(var number in grades) 
+            {
+              lowGrade = Math.Min(number, lowGrade);
+              highGrade = Math.Max(number, highGrade);  
+                result += number;
+            }
+            result /= grades.Count; 
+            Console.WriteLine($"the lowest grade is {lowGrade}");
+            Console.WriteLine($"The average grade is {result:N1}");
+
+        }
+
         private List<double> grades;
         private string name;
     }
--- Program.cs
+++ Program.cs
@@ -49,32 +49,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-         
-
             var book = new Book("Aryn's Gradebook");
             book.AddGrade(89.1);
             book.AddGrade(90.5);
-
-            
-           //initialization syntax
-           var grades = new List<double>() {12.7, 10.3, 6.11, 4.1 };
-           grades.Add(56.1);
-           
-            var result = 0.0;
-
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-
-            foreach(var number in grades) 
-            {
-              lowGrade = Math.Min(number, lowGrade);
-              highGrade = Math.Max(number, highGrade);  
-                result += number;
-            }
-            result /= grades.Count; 
-            Console.WriteLine($"the lowest grade is {lowGrade}");
-            Console.WriteLine($"The average grade is {result:N1}");
-
+            book.AddGrade(77.5);
+            book.ShowStatistics();
           
 
     }
